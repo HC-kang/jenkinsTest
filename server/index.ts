3ba6a4298bd513d558d94885ed6025ef98be8532
Ford <ford10082@gmail.com>
2021-12-05 17:46:41 +0900

    test

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('', (req, res, next) => {
-  res.send(200)
-})
+app.get("", (req, res, next) => {
+  res.send(200);
+  console.log("200");
+});
 
 app.listen(80, () => {
-  console.log('listening...')
-})
+  console.log("listening...");
+});
